9025a524850343481334ba63b0770332eb90b381
bastadevus <shreyas@basata.ai>
2024-08-18 16:04:38 -0700

    init commit

---
+++ src/components/Test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Test() {
+  return (
+    <div className="flex h-screen w-screen justify-center items-center">
+      <div className="flex items-center justify-center h-[50%] w-[50%] bg-cyan-500 opacity-50">
+        <div className="flex justify-center items-center h-max w-[50%]">
+        <p className="text-white">
+          Welcome to basata.ai
+        </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Test;
