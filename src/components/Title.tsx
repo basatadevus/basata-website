5b8529875f95864adba9273659a6485a64840a6e
bastadevus <shreyas@basata.ai>
2024-08-25 00:28:26 -0700

    misc update to fix

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { motion, useMotionValue, useTransform, useAnimation, AnimatePresence, animate } from "framer-motion";
 
 type TitleProps = {
